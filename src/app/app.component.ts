437db9dbd00e3069a2b9375acdeda5c1338faf8d
santhsatsangi <degala.prasanth92@gmail.com>
2024-02-01 17:45:16 +0530

    inital soon commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
